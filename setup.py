3d88ff384f5c8100d025cb92714353ae3435ca7b
Robert Gale <galer@ohsu.edu>
2020-10-22 16:08:20 -0700

    Version number to release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='strategery',
-    version='0.3.0a0',
+    version='0.3.0',
     author='Robert Gale',
     author_email='rcgale@gmail.com',
     packages=['strategery'],
